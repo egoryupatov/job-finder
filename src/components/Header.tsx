60f187c8d7fd1f3f5684cc4f76e786f6ee7f0ce9
Egor Iupatov <egoryupattov@gmail.com>
2025-01-01 20:01:53 +0300

    JBF-1: Добавлен хедер + мелкие доработки

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,23 @@
-export const Header = () => {
+import logo from "../assets/logo.png";
+
+export const Header: React.FC = () => {
   return (
     <div className="header__wrapper">
-      <div>Left part</div>
-      <div>Right part</div>
+      <div className="header__content">
+        <div className="header__content__left">
+          <div className="header__logo">
+            <img src={logo} width={40} />
+          </div>
+          <span>My CVs</span>
+          <span>Responses</span>
+          <span>Services</span>
+          <span>Help</span>
+        </div>
+        <div className="header__content__right">
+          <span>Search</span>
+          <span>New-York</span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { HomePage } from "./pages/HomePage.tsx";
 import { SearchPage } from "./pages/SearchPage.tsx";
 import { JobPage } from "./pages/JobPage.tsx";
 import "./styles/main.scss";
+import { PageLayout } from "./layouts/PageLayout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/job/:jobId" element={<JobPage />} />
+      <Route element={<PageLayout />}>
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/job/:jobId" element={<JobPage />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
